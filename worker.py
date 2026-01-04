8cf5e872a253958e80b7ac5c1223e18f8ea7dd68
Sharmila <jinka.s.learning@gmail.com>
2026-01-04 10:13:44 -0500

    testing stt

--- worker.py
+++ worker.py
@@ -5,12 +5,57 @@ openai_client = OpenAI()
 
 
 def speech_to_text(audio_binary):
+    base_url = "https://sn-watson-stt.labs.skills.network"
+    api_url = base_url+'/speech-to-text/api/v1/recognize'
+
+    params = {
+        'model': 'en-US_Multimedia',
+    }
+
+    response = requests.post(api_url, params=params, data=audio_binary).json()
+
+    while bool(response.get('requests')):
+        print('speech to text response:', response)
+        text = response.get('results').pop().get('alternatives').pop().get('transcript')
+        print('recognised text: ', text)
+        return text
+
     return None
 
 
 def text_to_speech(text, voice=""):
-    return None
+    base_url = "https://sn-watson-tts.labs.skills.network"
+    api_url = base_url + '/text-to-speech/api/v1/synthesize?output=output_text.wav'
+
+    if voice != "" and voice != "default":
+        api_url += "&voice=" + voice
+    # Set the headers for our HTTP request
+    headers = {
+        'Accept': 'audio/wav',
+        'Content-Type': 'application/json',
+    }
+    # Set the body of our HTTP request
+    json_data = {
+        'text': text,
+    }
+    response = requests.post(api_url, headers=headers, json=json_data)
+    print('text to speech response:', response)
+    return response.content
 
 
 def openai_process_message(user_message):
-    return None
+    # Set the prompt for OpenAI Api
+    prompt = "Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. Keep responses concise - 2 to 3 sentences maximum."
+    # Call the OpenAI Api to process our prompt
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-5-nano", 
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_message}
+        ],
+        max_completion_tokens=1000
+    )
+    print("openai response:", openai_response)
+    # Parse the response to get the response message for our prompt
+    response_text = openai_response.choices[0].message.content
+    return response_text
